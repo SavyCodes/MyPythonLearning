286f3e6c007fa1c9610f33186a68094c6a030591
sarang <sarangafle@gmail.com>
2023-05-27 12:40:44 +0530

    first Commit

---
+++ corey schafer/String.py
@@ -0,0 +1,25 @@
+# Print Welcome Message
+print('Hello World')
+
+# Create a Variable
+message = 'Hello World'
+print(message)
+
+# To resolve mutiple quote issue 
+print("Hello's world")
+
+# Length of Variable
+print(len(message))
+
+# Access Index of Characters
+print(message[0:5])
+
+# Slicing
+print(message[6:])
+
+# All Lower Case, Upper Case
+print(message.lower())
+print(message.upper())
+
+# Count
+print(message.count('Hello'))
